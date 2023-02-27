c7cf13e8dd87670e0e88833b0887e226a5c735da
Rick Beerendonk <rick@beerendonk.com>
2023-02-27 11:01:19 +0100

    Import examples from react-examples.

---
+++ 3. CSS - i. Global/pages/index.js
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import MyText from './MyText.js';
+
+function Index() {
+  return (
+    <h1>
+      Hello <MyText text="Next.js" />!
+    </h1>
+  );
+}
+
+export default Index;
