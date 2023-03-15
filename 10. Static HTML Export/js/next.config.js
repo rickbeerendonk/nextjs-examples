98cdcf69a96c731aa49540483a67b507a61c422b
Rick Beerendonk <rick@beerendonk.com>
2023-03-15 23:24:48 +0100

    Add "Static HTML Export" example.

---
+++ 10. Static HTML Export/js/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  // 2. Static HTML Export
+  output: 'standalone'
+};
+
+module.exports = nextConfig;
---
+++ 10. Static HTML Export/js/pages/page2.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+function Page2() {
+  return <h1>Page 2</h1>;
+}
+
+export default Page2;
