5ec0aba790a4d92029dec71a254a0c4099e91e3c
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 04:00:06 +0100

    Add "Render Conditionally - if-else - Multiple Elements" example.

---
+++ 04 - Render Conditionally/c. if-else - ii. Multiple Elements/js/pages/index.js
@@ -0,0 +1,25 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+function Index() {
+  const show = false;
+
+  return (
+    <>
+      <h3>Conditional:</h3>
+      {show ? (
+        <>
+          <div>Invisible</div>
+          <div>Also Invisible</div>
+        </>
+      ) : (
+        <>
+          <div>Visible</div>
+          <div>Also Visible</div>
+        </>
+      )}
+    </>
+  );
+}
+
+export default Index;
