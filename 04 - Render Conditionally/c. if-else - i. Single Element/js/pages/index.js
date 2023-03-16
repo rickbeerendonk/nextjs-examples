776361321c980d41e30efff6eff4703b31cc7181
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 04:00:21 +0100

    Add "Render Conditionally - if-else - Single Element" example.

---
+++ 04 - Render Conditionally/c. if-else - i. Single Element/js/pages/index.js
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+function Index() {
+  const show = false;
+
+  return (
+    <>
+      <h3>Conditional:</h3>
+      {show ? <div>Invisible</div> : <div>Visible</div>}
+    </>
+  );
+}
+
+export default Index;
