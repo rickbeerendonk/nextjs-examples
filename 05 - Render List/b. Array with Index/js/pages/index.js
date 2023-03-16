a9643a09c70dd99a31626201bef94b06c572e09b
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 03:42:28 +0100

    Add "Render List - Array with Index" example.

---
+++ 05 - Render List/b. Array with Index/js/pages/index.js
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+function Index() {
+  const names = ['Alexandra', 'Benjamin', 'Charlotte'];
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <ol>
+        {
+          // Prefer map (instead of for-loops):
+          names.map((name, index) => (
+            <li key={name} value={index}>
+              Hello {name}
+            </li>
+          ))
+        }
+      </ol>
+    </>
+  );
+}
+
+export default Index;
