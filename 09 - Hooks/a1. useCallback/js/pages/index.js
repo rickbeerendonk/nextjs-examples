44b79044c3f48e87eda0a3cc4a015027fa144a18
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 03:54:29 +0100

    Add "Hooks - useCallback" examples.

---
+++ 09 - Hooks/a1. useCallback/js/pages/index.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import Parent from '../components/Parent';
+
+function Index() {
+  return <Parent />;
+}
+
+export default Index;
