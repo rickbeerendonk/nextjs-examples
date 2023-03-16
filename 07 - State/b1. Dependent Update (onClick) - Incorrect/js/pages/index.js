095357b97e97dacc15774b3bc8a87e004713737d
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 03:17:17 +0100

    Add "State - Dependent Update" example.

---
+++ 07 - State/b1. Dependent Update (onClick) - Incorrect/js/pages/index.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import ClickMe from '../components/ClickMe';
+
+function Index() {
+  return <ClickMe />;
+}
+
+export default Index;
