ef51f7b681a71d7c5193db9dd7d15abb34a4bfbd
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 03:46:19 +0100

    Add "State - No XSS Protection" example.

---
+++ 07 - State/d. No XSS Protection/js/pages/index.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import GreetingEditor from '../components/GreetingEditor';
+
+function Index() {
+  return <GreetingEditor />;
+}
+
+export default Index;
