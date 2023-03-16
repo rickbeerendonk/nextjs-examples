318d8a05c2f23e1a5bb351815e32a2037c2c7541
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 19:28:43 +0100

    Add "Context - Without" example.

---
+++ 11 - Context/a. Without/js/pages/index.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import Top from '../components/Top';
+
+function Home() {
+  return <Top color="red" />;
+}
+
+export default Home;
