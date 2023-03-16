093f71211bf91e8648d12b24af4b3f3c1948594e
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 21:33:59 +0100

    Add "CSS - Component" example.

---
+++ 03 - CSS/c. Component/js/components/MyTitle.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import styles from './MyTitle.module.css';
+
+function MyTitle() {
+  return <h1 className={styles.title}>Hello Next.js!</h1>;
+}
+
+export default MyTitle;
---
+++ 03 - CSS/c. Component/js/pages/index.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import MyTitle from '../components/MyTitle';
+
+function Home() {
+  return <MyTitle />;
+}
+
+export default Home;
