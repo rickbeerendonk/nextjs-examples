536b5c859eb3adb3cfa816ac5cfac66d89974420
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 01:08:17 +0100

    Simplification.

--- 02. CSS/a. Global/js/pages/index.js
+++ 02. CSS/a. Global/js/pages/index.js
@@ -1,14 +1,8 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-import MyText from './MyText.js';
-
 function Index() {
-  return (
-    <h1>
-      Hello <MyText text="Next.js" />!
-    </h1>
-  );
+  return <h1>Hello Next.js!</h1>;
 }
 
 export default Index;
--- 02. CSS/b. Local/js/pages/index.js
+++ 02. CSS/b. Local/js/pages/index.js
@@ -1,16 +1,10 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-import MyText from './MyText.js';
-
 import styles from './index.module.css';
 
 function Index() {
-  return (
-    <h1 className={styles.hello}>
-      Hello <MyText text="Next.js" />!
-    </h1>
-  );
+  return <h1 className={styles.hello}>Hello Next.js!</h1>;
 }
 
 export default Index;
