01b09dfff5d037f5c8d78e640aad9a5c110c4af6
Rick Beerendonk <rick@beerendonk.com>
2023-03-16 05:11:55 +0100

    Remove all not in part-1.

--- 20 - Pages/a. Page - i. Basics/js/pages/index.js
+++
@@ -1,8 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2017 Rick Beerendonk          !*/
-
-function Index() {
-  return <h1>Hello Next.js!</h1>;
-}
-
-export default Index;
--- 23 - Static HTML Export/js/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  // 2. Static HTML Export
-  output: 'standalone'
-};
-
-module.exports = nextConfig;
