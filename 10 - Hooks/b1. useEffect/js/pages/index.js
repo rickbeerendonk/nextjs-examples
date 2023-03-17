4b67c31f43848d514512033fc77f98c0cb3fa42d
Rick Beerendonk <rick@beerendonk.com>
2023-03-17 01:16:20 +0100

    Add "Hooks - useEffect" example.

---
+++ 10 - Hooks/b1. useEffect/js/pages/index.js
@@ -0,0 +1,23 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+import MousePosition from '../components/MousePosition';
+
+function Home() {
+  const [visible, setVisible] = React.useState(true);
+
+  function handleShowHide() {
+    setVisible(v => !v);
+  }
+
+  return (
+    <React.Fragment>
+      <button onClick={handleShowHide}>{visible ? 'Hide' : 'Show'}</button>
+      {visible && <MousePosition />}
+    </React.Fragment>
+  );
+}
+
+export default Home;
