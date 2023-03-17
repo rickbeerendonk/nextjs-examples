c0ea0845d29a60bf130d9679683fd936edeb0c35
Rick Beerendonk <rick@beerendonk.com>
2023-03-17 03:13:24 +0100

    Reorder.

---
+++ 18 - getServerSideProps/b. Fetch/js/pages/index.js
@@ -0,0 +1,27 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import PostList from '../components/PostList';
+
+function Home({ posts }) {
+  return (
+    <>
+      <h1>Posts</h1>
+      <PostList posts={posts} />
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  const json = await response.json();
+
+  return {
+    props: { posts: json }
+  };
+}
+
+export default Home;
