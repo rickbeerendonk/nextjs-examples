b0e110f5c49576f6dcff16564fbc64d03d948686
Rick Beerendonk <rick@beerendonk.com>
2024-12-03 22:19:34 +0100

    Add app-router-version.

---
+++ 12 - Head Title/app-router/ts/app/page.tsx
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+export default function Home() {
+  return (
+    <>
+      <title>Hello Title</title>
+      <meta name="Demo" content="Hello Meta" />
+      <h1>Hello Next.js!</h1>
+    </>
+  );
+}
