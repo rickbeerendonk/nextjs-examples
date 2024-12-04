1ad64dbf77eca3e2520e80c854c2bbf16763fa36
Rick Beerendonk <rick@beerendonk.com>
2024-12-04 02:43:39 +0100

    Add dynamic links.

--- 15 - Pages/c. Dynamic Routes/app-router/ts/app/page/[id]/page.tsx
+++ 15 - Pages/c. Dynamic Routes/app-router/ts/app/page/[id]/page.tsx
@@ -1,12 +1,19 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2024 Rick Beerendonk          !*/
 
+import Link from 'next/link';
+
 type PageProps = {
   params: Promise<{ id: string }>;
 };
 
 // Async!
 export default async function Page({ params }: PageProps) {
-  const id = (await params).id;
-  return <h1>Page {id}</h1>;
+  const id = Number((await params).id);
+  return (
+    <>
+      <h1>Page {id}</h1>
+      <Link href={`${id + 1}`}>Go to Page {id + 1}</Link>
+    </>
+  );
 }
