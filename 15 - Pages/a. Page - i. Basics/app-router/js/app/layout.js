1fedc07f6eb63aa2d694ee37293df2ab95a88490
Rick Beerendonk <rick@beerendonk.com>
2024-12-04 01:47:42 +0100

    Fix example.

--- 15 - Pages/a. Page - i. Basics/app-router/js/app/layout.js
+++ 15 - Pages/a. Page - i. Basics/app-router/js/app/layout.js
@@ -1,18 +1,12 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2024 Rick Beerendonk          !*/
 
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Demo',
   description: 'Demo app'
 };
 
-export default function RootLayout({
-  children
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
--- 15 - Pages/a. Page - i. Basics/app-router/ts/app/page.tsx
+++ 15 - Pages/a. Page - i. Basics/app-router/ts/app/page.tsx
@@ -1,12 +1,6 @@
-export default function Home() {
-  const name = 'React';
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
 
-  return (
-    <ol>
-      <li>Use value: {name}</li>
-      <li>Use expression: {'hello '.toUpperCase() + 2023}</li>
-      <li>Skip compilation 1: {'{name}'}</li>
-      <li>Skip compilation 2: &#123;name&#125;</li>
-    </ol>
-  );
+export default function Home() {
+  return <h1>Hello Next.js!</h1>;
 }
