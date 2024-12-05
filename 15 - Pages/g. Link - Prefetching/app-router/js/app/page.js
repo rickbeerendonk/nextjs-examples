d9c3489805d29380a4e5e7e52bbd2539a9f1d5ac
Rick Beerendonk <rick@beerendonk.com>
2024-12-05 02:50:30 +0100

    Add prefetching demo.

---
+++ 15 - Pages/g. Link - Prefetching/app-router/js/app/page.js
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+import Link from 'next/link';
+
+// <Link> allows for:
+// - Client-side navigation
+// - Prefetching
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="sub1">Go to Page 1</Link>
+      <div style={{ height: '2000px' }}></div>
+      <Link href="sub2">Go to Page 2</Link>
+    </>
+  );
+}
