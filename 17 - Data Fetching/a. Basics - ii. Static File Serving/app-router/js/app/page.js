22d1c771a13c2f4793e39a53a0a049c5b8a9159f
Rick Beerendonk <rick@beerendonk.com>
2024-12-06 06:13:57 +0100

    Add comment

--- 17 - Data Fetching/a. Basics - ii. Static File Serving/app-router/js/app/page.js
+++ 17 - Data Fetching/a. Basics - ii. Static File Serving/app-router/js/app/page.js
@@ -1,7 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2024 Rick Beerendonk          !*/
 
+// Async
 export default async function Home() {
+  // f.e. use for Headless CMS
   let data = await fetch('https://jsonplaceholder.typicode.com/posts');
   let posts = await data.json();
   return (
