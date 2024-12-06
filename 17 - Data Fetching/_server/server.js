e0d5a6121bcb4c4375eb35d3f75300e835f41f0a
Rick Beerendonk <rick@beerendonk.com>
2024-12-06 07:05:57 +0100

    Add basic caching examples.

---
+++ 17 - Data Fetching/_server/server.js
@@ -0,0 +1,29 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+const http = require('http');
+
+let requestCount = 0;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    const response = {
+      name: 'Server',
+      time: new Date().toISOString(),
+      requestNr: ++requestCount
+    };
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(JSON.stringify(response));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+// Start the server
+const PORT = 3100;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ 17 - Data Fetching/c. Cache - ii. With components/app-router/js/app/page.js
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+import ServerData from './components/ServerData';
+
+export default async function Page() {
+  let res = await fetch('http://localhost:3100');
+  let obj = await res.json();
+  return (
+    <>
+      <h1>Fetched at {new Date().toLocaleTimeString()}</h1>
+      <p style={{ color: 'gray' }}>
+        Start the server in the _server folder first
+      </p>
+      <div>Server response</div>
+      <ul>
+        <li>Name: {obj.name}</li>
+        <li>Time: {obj.time}</li>
+        <li>Request number: {obj.requestNr}</li>
+      </ul>
+      <ServerData />
+    </>
+  );
+}
