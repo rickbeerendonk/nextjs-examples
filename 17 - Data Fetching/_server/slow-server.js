a53eb348c0d52ed5b416545e4d65d4367467cad9
Rick Beerendonk <rick@beerendonk.com>
2024-12-06 17:59:43 +0100

    Add slow server.

---
+++ 17 - Data Fetching/_server/slow-server.js
@@ -0,0 +1,44 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+const http = require('http');
+
+let requestCount = 0;
+
+const server = http.createServer((req, res) => {
+  // Set CORS headers for all requests
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Specify allowed HTTP methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
+
+  // Handle OPTIONS preflight requests
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  if (/* req.method === 'GET' && */ req.url === '/') {
+    // Slow response
+    setTimeout(() => {
+      const response = {
+        name: 'Server',
+        time: new Date().toISOString(),
+        requestNr: ++requestCount
+      };
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+
+      res.end(JSON.stringify(response));
+    }, 3000);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+// Start the server
+const PORT = 3100;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
