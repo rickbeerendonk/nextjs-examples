4ca7f96c4688d7733059743c602e31175df02fa6
Rick Beerendonk <rick@beerendonk.com>
2025-03-07 02:59:01 +0100

    Fix example.

--- 15 - Pages/a. Root Route - ii. With Component/app-router/js/app/page.js
+++ 15 - Pages/a. Root Route - ii. With Component/app-router/js/app/page.js
@@ -1,6 +1,12 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2024 Rick Beerendonk          !*/
 
+import MyText from './components/MyText.jsx';
+
 export default function Home() {
-  return <h1>Hello Next.js!</h1>;
+  return (
+    <h1>
+      Hello <MyText text="Next.js" />!
+    </h1>
+  );
 }
