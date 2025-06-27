2ae1efd0d46622bea571f1e979eef39f4c33c682
Rick Beerendonk <rick@beerendonk.com>
2025-06-27 12:00:10 +0200

    Add Manifest demos.

---
+++ 18 - Manifest/a. Manifest File/app-router/ts/app/page.tsx
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+import styles from './page.module.css';
+
+export default function Home() {
+  return <div className={styles.page}>Hello Next.js</div>;
+}
